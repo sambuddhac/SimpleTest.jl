407f0658c153dda3c011a007618fadfa923e2f8c
sambuddhac <sambuddha.chakrabarti@gmail.com>
2021-05-25 16:27:30 -0600

    Corrected run for successful test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ using Test
 @testset "SimpleTest.jl" begin
     @test simple_operation(2.0, 3.0) == 5
     @test simple_operation(2.1, 3.1)==5.2
-    @test simple_operation(21.0, 31.0)== 53.0
+    @test simple_operation(21.0, 31.0)== 52.0
 end
